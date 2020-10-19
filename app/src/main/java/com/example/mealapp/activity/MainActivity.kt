d6529ae1a05850684693396aee18a10198387ac8
yang0925 <keun0602@naver.com>
2020-10-19 16:13:50 +0900

    [Update] 업데이트 UI

--- app/src/main/java/com/example/mealapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/mealapp/activity/MainActivity.kt
@@ -13,6 +13,7 @@ import com.example.mealapp.fragment.LunchFragment
 import com.example.mealapp.network.RetrofitClient
 import com.example.mealapp.network.Service
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_search.*
 import kotlinx.android.synthetic.main.fragment_breakfast.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -23,7 +24,6 @@ import java.time.format.DateTimeFormatter
 
 
 class MainActivity : AppCompatActivity(){
-
 
     var schoolId : String = ""
     var opCode : String = ""
@@ -47,9 +47,8 @@ class MainActivity : AppCompatActivity(){
 
         setUpTabs()
         setMealsRetrofit()
+
     }
-
-
 
     private fun setUpTabs() {
         val adapter = ViewPagerAdapter(supportFragmentManager)
@@ -59,9 +58,9 @@ class MainActivity : AppCompatActivity(){
         viewPager.adapter = adapter
         tabs.setupWithViewPager(viewPager)
 
-        tabs.getTabAt(0)!!.setIcon(R.drawable.ic_baseline_brightness_medium_24)
-        tabs.getTabAt(1)!!.setIcon(R.drawable.ic_baseline_brightness_high_24)
-        tabs.getTabAt(2)!!.setIcon(R.drawable.ic_baseline_brightness_3_24)
+        //tabs.getTabAt(0)!!.setIcon(R.drawable.ic_baseline_brightness_medium_24)
+        //tabs.getTabAt(1)!!.setIcon(R.drawable.ic_baseline_brightness_high_24)
+        //tabs.getTabAt(2)!!.setIcon(R.drawable.ic_baseline_brightness_3_24)
     }
 
     private fun setMealsRetrofit() {
@@ -79,7 +78,7 @@ class MainActivity : AppCompatActivity(){
                 Toast.makeText(applicationContext, "성공", Toast.LENGTH_SHORT).show()
                 Log.d("TAG", response.body()!!.data.meals[0].replace("<br/>", "\n"))
                 tv_breakfast.text = response.body()!!.data.meals[0].replace("<br/>", "\n")
-                tv_date.text = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일").format(current);
+                //tv_date.text = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일").format(current);
             }
 
         })
